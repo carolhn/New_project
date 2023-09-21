a46fef324dc440950c353bb742d4a0b93d563a15
Caroline Nunes <nunescaroline905@gmail.com>
2023-09-21 17:50:33 -0300

    feat: adding handle error

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,16 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+import { handleErrors } from './middlewares/handleErrors';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
+app.use(handleErrors);
 
-app.listen(3333, () => console.log('Server is running on port 3333 🚀'));
-
+app.listen(3333, () => {
+  console.log('Server is running on port 3333 🚀');
+});
